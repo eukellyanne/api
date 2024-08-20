1af99f4f5ec560a5c3ef0e2c70d092aaf5ec337f
kellyanne karen <Kellyannekaren23@gmail.com>
2024-08-20 15:10:02 -0300

    Initial commit

---
+++ src/main/java/br/com/meunome/api/ExibicaoDoNomeApplication.java
@@ -0,0 +1,13 @@
+package br.com.meunome.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExibicaoDoNomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExibicaoDoNomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meunome/api/ExibicaoDoNomeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meunome.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExibicaoDoNomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
